123f3f67093bdb9674d5ec26404c8f86c4e84ffa
emar <49133270+emarron@users.noreply.github.com>
2022-08-03 21:48:57 -0400

    added tex files

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from parsers.dds import Dds
+
 # just tests my parsers
 print("hello")
-f = Dds.from_file("squidward-256-BGRA_32.dds")
-f1 = Dds.from_file("squidward-256-BC1.dds")
+f = Dds.from_file("images/dds/squidward-256-BGRA_32.dds")
+f1 = Dds.from_file("images/dds/squidward-256-BC1.dds")
+
 print(str(f.hdr.flags) + " BGRA file")
 print(str(f1.hdr.flags) + " BC1 file")
+
+if "mipmapcount" in str(f1.hdr.flags):
+    print("mipmaps true")
+else:
+    print("mipmaps false")
